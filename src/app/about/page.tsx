8d4a62ac90e63f0ea54f0d67ee334940b09dae25
Nonaka <nonaka-yuuki810@g.ecc.u-tokyo.ac.jp>
2025-08-12 19:39:37 +0900

    add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <div className="p-6">
+        <div className="text-xl mb-4">予定一覧</div>
+        <ul>
+          <li>8/8（金） 会議</li>
+          <li>8/9（土） ランチ</li>
+        </ul>
+      </div>
+  );
+}
